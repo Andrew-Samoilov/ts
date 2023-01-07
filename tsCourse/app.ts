35a0a67d3f0e43c4ce441893fc36579f0e050a7b
Andrew Samoilov <veprsag@gmail.com>
2023-01-07 20:10:42 +0200

    start

--- tsCourse/app.ts
+++ tsCourse/app.ts
@@ -1,11 +1,78 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+let age:number;
+age = 50;
+let name:string= 'Max';
+let toggle: boolean;
+toggle = true;
+let empty:null;
+empty = null;
+let notInitialize:undefined;
+notInitialize = undefined;
+let callback = (a: number) => number;
+callback = (a) => { return 100 + a };
 
-function add(num1: number, num2: number) {
+let anything:any;
+anything = -20;
+anything = 'Text';
+anything = {};
+
+let some:unknown;
+some = 'Text';
+
+let str: string;
+
+if (typeof some === 'string') {
+  str = some;
+}
+
+let str: string;
+
+let person: [string, number];
+person = ['Max', 21];
+
+enum Load {LOADING, READY};
+
+const page = {
+  load: Load.READY
+}
+
+if (page.load === Load.LOADING) {
+  console.log('Сторінка завантажується');
+}
+if (page.load === Load.READY) {
+  console.log('Сторінка завантажена');
+}
+
+let union: string | number;
+
+let literal: 'enable' | 'disable';
+
+function showMessage(message:string):void {
+  console.log(message);
+}
+
+
+function calc(num1:number, num2:number):number {
   return num1 + num2;
 }
 
-button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
-});
+function customError():never {
+  throw new Error('Error');
+}
+
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: 'open' | 'close';
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  }
+}
+
+const page2 = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
