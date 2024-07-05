1340d8cc3f09b23fb831c10c3df1d0a99b354752
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-07-05 23:01:06 +0300

    add: 8 kuy kata

--- codewars-done/cw-8-sh-l-sh.ts
+++ codewars-done/cw-8-sh-l-sh.ts
@@ -1,16 +1,15 @@
-console.log(` - *** 8 kuy * Short Long Short * `);
+console.log(` - * 8 kuy * Short Long Short * `);
 
 function shortLongShort(a: string, b: string) {
     // let res = '';
-
     // if (a.length > b.length) {
     //     res = b + a + b 
     // } else {
     //     res = a + b + a; 
     // }
     // return res;
+    
     return a.length > b.length ? b + a + b : a + b + a;
-
 }
 
 console.log(shortLongShort('45', '1'), '1451');
