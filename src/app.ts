d60c866512aa2ae8dc8081b282ad84ae8caafa45
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-07-08 22:58:54 +0300

    in propgress

--- src/app.ts
+++ src/app.ts
@@ -13,19 +13,26 @@ function isInteresting(n: number, awesomePhrases: number[]): number {
             return res;
         }
         if ((awesomePhrases[index] - n) <= 2 && (awesomePhrases[index] - n)>0) {
-            res = 1;
-            
+            res = 1;            
             // console.log(awesomePhrases[index], n);
             return res;
         }
     }
 
     let foo = 0;
+    
+    //all zero exept 1st 1000, 90000
     foo = Number((n + ' ').slice(1, -1));
     // console.log(n, foo);
-
-    //all zero exept 1st 1000, 90000
     if (n > 9 && foo === 0) {
+        res = 2;
+        return res;
+    }
+
+    //all symbol the same, like 1111 or 88888
+    let arr = String(n).split('');
+        // console.log(n, arr, arr.every((currentValue: string) => currentValue === arr[0]));
+    if (arr.every((currentValue: string) => currentValue === arr[0])) {
         res = 2;
         return res;
     }
@@ -34,11 +41,12 @@ function isInteresting(n: number, awesomePhrases: number[]): number {
 }
 
 
-console.log(isInteresting(3, [1337, 256]), 0);
-console.log(isInteresting(1336, [1337, 256]), 1);
-console.log(isInteresting(1337, [1337, 256]), 2);
-console.log(isInteresting(11208, [1337, 256]), 0);
+// console.log(isInteresting(3, [1337, 256]), 0);
+// console.log(isInteresting(1336, [1337, 256]), 1);
+// console.log(isInteresting(1337, [1337, 256]), 2);
+// console.log(isInteresting(11208, [1337, 256]), 0);
 // console.log(isInteresting(11209, [1337, 256]), 1);
 // console.log(isInteresting(11211, [1337, 256]), 2);
 
-console.log(isInteresting(10000, [1337, 256]), 2);
+// console.log(isInteresting(90000, [1337, 256]), 2);
+console.log(isInteresting(7777737, [1337, 256]), 2);
